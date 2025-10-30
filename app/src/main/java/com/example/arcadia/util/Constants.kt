552b042c44e6b95327c69f6b9bbea98f27f7c868
Ahmed <ahmedbobos.rashid@gmail.com>
2025-10-30 21:50:46 +0300

    Implement splash screen and add custom button components

---
+++ app/src/main/java/com/example/arcadia/util/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.arcadia.util
+
+object Constants {
+    const val ON_BOARDING_PAGE_COUNT = 3
+    const val LAST_ON_BOARDING_PAGE = 2
+    
+    const val PREFERENCES_NAME = "arcadia_preferences"
+    const val PREFERENCES_KEY = "on_boarding_completed"
+
+    const val ALPHA_FULL = 1f
+    const val ALPHA_HALF = 0.5f
+    const val ALPHA_DISABLED = 0.38F
+    const val ALPHA_TWENTY = 0.2f
+    const val AlPHA_TEN_PERSENT = 0.1F
+    const val ALPHA_ZERO = 0f
+
+    const val MAX_QUANTITY = 10
+    const val MIN_QUANTITY = 1
+}
+
+
